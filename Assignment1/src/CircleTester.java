71767b6df02f5fc83b4316ac97fb06beee8221c2
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-09-27 16:58:22 +0800

    finish others tester class

--- Assignment1/src/CircleTester.java
+++ Assignment1/src/CircleTester.java
@@ -4,5 +4,21 @@
  */
 
 public class CircleTester {
-    
+    public static void main(String[] args){
+        System.out.println("Start to testing Circle class");
+        System.out.println("-----------------------------");
+
+        Circle c = new Circle();
+        System.out.println("a new Circle object is created and c is the reference variable");
+        c.setVertices(3.14d);
+        int[] cVerticeX = new int[2];
+        int[] cVerticeY = new int[2];
+        System.out.println("The \"Vertices\" of this Square bounding Circle is (" + 
+                            cVerticeX[0] + ',' + cVerticeY[0] + ") (" +
+                            cVerticeX[1] + ',' + cVerticeY[1] + ')' +
+                            ", It access both 3 overwritten methods of Circle class.");
+
+        System.out.println("----------------------------");
+        System.out.println("The end testing Circle class");
+    }
 }
--- Assignment1/src/TriangleTester.java
+++ Assignment1/src/TriangleTester.java
@@ -4,5 +4,24 @@
  */
 
 public class TriangleTester {
-    
+    public static void main(String[] args){
+        System.out.println("Start to testing Triangle class");
+        System.out.println("-------------------------------");
+
+        Triangle t = new Triangle();
+        System.out.println("a new Triangle object is created with t reference variable");
+
+        t.setVertices(3.14d);
+        int[] tVerticeX = t.getX();
+        int[] tVerticeY = t.getY();
+        System.out.println("It's vertices is set to (" + 
+                            tVerticeX[0] + ',' + tVerticeY[0] + ") (" +
+                            tVerticeX[1] + ',' + tVerticeY[1] + ") (" +
+                            tVerticeX[2] + ',' + tVerticeY[2] + ')' +
+                            ", It is the only overwritten class in Triangle class");
+
+        System.out.println("------------------------------");
+        System.out.println("The end testing Triangle class");
+
+    }
 }
