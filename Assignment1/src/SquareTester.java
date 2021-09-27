3dc8b449c4504fcae0ca4e46ceed2ad0944fafb9
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-09-27 15:47:19 +0800

    Squaretester

--- Assignment1/src/SquareTester.java
+++ Assignment1/src/SquareTester.java
@@ -4,5 +4,19 @@
  */
 
 public class SquareTester {
-    
+    public static void main(String[] args){
+        System.out.println("Start to testing Square class");
+        System.out.println("-----------------------------");
+
+        Square sq = new Square();
+        System.out.println("a new Square object is created and sq is it's reference variable");
+
+        sq.setVertices(3.14d);
+        int[] sideLengths = sq.getX();
+        System.out.println("The Vertice is set to " + sideLengths[0] + " each side of the square, it's only overwriteen parameters in square class");
+        
+        System.out.println("----------------------------");
+        System.out.println("The end testing Square class");
+
+    }
 }
