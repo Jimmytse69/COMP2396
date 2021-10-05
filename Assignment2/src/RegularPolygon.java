5eda4374aba9fc6c61a99c16c7fb035715b91668
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-10-05 10:34:17 +0800

    Shape and Polygon try

--- Assignment2/src/RegularPolygon.java
+++ Assignment2/src/RegularPolygon.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 /**
  * This the RegularPolygon class to model Regular Polygon shapes under the inheritance of all shapes instance variable and methods
  * there are extra methods/variable need to be overwritten stated as follow. It have 3 Constructors to create this object from class:
@@ -9,29 +8,92 @@ import java.awt.Color;
 
 public class RegularPolygon extends Shape{
     public RegularPolygon(){};
-    public RegularPolygon(int n){};
-    public RegularPolygon(int n, double r){};
+    public RegularPolygon(int n){
+        this.numOfSides = n;
+    };
+    public RegularPolygon(int n, double r){
+        this.numOfSides = n;
+        this.radius = r;
+    };
 
     private int numOfSides;
     private double radius;
 
     public int getNumOfSides(){
-        return numOfSides;
+        return this.numOfSides;
     }
     public double getRadius(){
-        return radius;
+        return this.radius;
     }
     public void setNumOfSides(int n){
+        if (n <= 3){
+            this.numOfSides = 3;
+        }
+        else{
+            this.numOfSides = n;
+        }
+        double[] ResetXLocal = new double[this.getXLocal().length];     //Reset Xlocal
+        for (int i = 0; i < ResetXLocal.length; i++){
+            ResetXLocal[i] = 0;
+        }
+        double[] ResetYLocal = new double[this.getYLocal().length];     //Reset Ylocal
+        for (int i = 0; i < ResetYLocal.length; i++){
+            ResetYLocal[i] = 0;
+        }
 
     }
     public void setRadius(double r){
-
+        if (r <= 0){
+            this.radius = 0;
+        }
+        else{
+            this.radius = r;
+        }        
+        double[] ResetXLocal = new double[this.getXLocal().length];     //Reset Xlocal
+        for (int i = 0; i < ResetXLocal.length; i++){
+            ResetXLocal[i] = 0;
+        }
+        double[] ResetYLocal = new double[this.getYLocal().length];     //Reset Ylocal
+        for (int i = 0; i < ResetYLocal.length; i++){
+            ResetYLocal[i] = 0;
+        }
     }
     public void setVertices(){
+        final double theta = 2*(Math.PI)/numOfSides;
+        final double alphaOdd = 0;
+        final double alphaEven = Math.PI/numOfSides;
 
+        if (this.numOfSides % 2 == 1){      //odd sides case
+            for (int i = 0; i < numOfSides; i++){
+                this.getXLocal()[i] = this.getRadius()*(Math.cos(alphaOdd - i*theta));
+                this.getYLocal()[i] = this.getRadius()*(Math.sin(alphaOdd - i*theta));
+            }
+        }
+        else{
+            for (int i = 0; i < numOfSides; i++){
+                this.getXLocal()[i] = this.getRadius()*(Math.cos(alphaEven - i*theta));
+                this.getYLocal()[i] = this.getRadius()*(Math.sin(alphaEven - i*theta));
+            }
+        }
     }
+    //target find min(leftest) x coordinate of vertices, than use the method in appendix
     public boolean contains(double x, double y){
-        return false;
+        double[] xL = this.getXLocal();
+        double minX = xL[0];
+        for (int i = 0; i < xL.length; i++){
+            if (xL[i] < minX){
+                minX = xL[i];
+            }
+        }
+        for (int i = 0; i < (xL.length - 1); i++){
+            if (x < minX){
+                return false;   //if it is even lefter than leftest x Vertices, it must outside
+            }
+            else{
+                x = x*Math.cos(2*Math.PI/numOfSides) - y*Math.sin(2*Math.PI/numOfSides);    //rotate the point
+            }
+        }
+        return true;
     }
-
+    
 }
--- Assignment2/src/Shape.java
+++ Assignment2/src/Shape.java
@@ -9,9 +9,9 @@ import java.awt.Color;
 public class Shape {
     private Color color;
     private boolean filled;
-    private double theta;
-    private double xc;
-    private double yc;
+    private double theta;   //radian, Screen Coordinate system(SCS)
+    private double xc;  //SCS
+    private double yc;  //SCS
     private double[] xLocal;
     private double[] yLocal;
 
@@ -31,46 +31,67 @@ public class Shape {
         return this.yc;
     }
     public double[] getXLocal(){
-        return xLocal;
+        return this.xLocal;
     }
-    public double[] getYlocal(){
-        return yLocal;
+    public double[] getYLocal(){
+        return this.yLocal;
     }
 
     public void setColor(Color color){
-
+        this.color = color;
     }
     public void setFilled(boolean filled){
-
+        this.filled = filled;
     }
     public void setTheta(double theta){
-
+        this.theta = theta;
     }
     public void setXc(double xc){
-
+        this.xc = xc;
     }
     public void setYc(double yc){
-
+        this.yc = yc;
     }
     public void setXLocal(double[] xLocal){
-
+        this.xLocal = xLocal;
     }
     public void setYLocal(double[] yLocal){
-
+        this.yLocal = yLocal;
     }
     public void translate(double dx, double dy){
-
+        xc += dx;
+        yc += dy;
     }
     public void rotate(double dt){
+        theta += dt;
+    }
 
-    }
+    /**
+     * a method for retrieving the x-coordinates of the vertices (in counter-clockwise order).
+     * @return screen coordinate of X vertices of the shape rounded to nearest intergers.
+     */
     public int[] getX(){
-        int[] dummy = new int[3];
-        return dummy;
+        int[] xr = new int[xLocal.length];
+        double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
+        for (int i = 0; i < xLocal.length; i++){
+            xr[i] = (int) Math.round(xLocal[i] * cosTheta - yLocal[i] * sinTheta + xc);
+        }
+        return xr;
     }
+
+    /**
+     * a method for retrieving the y-coordinates of the vertices (in counter-clockwise order).
+     * @return screen coordinate of Y vertices of the shape rounded to nearest intergers.
+     */
     public int[] getY(){
-        int[] dummy = new int[3];
-        return dummy;
+        int[] yr = new int[yLocal.length];
+        double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
+        for (int i = 0; i < yLocal.length; i++){
+            yr[i] = (int) Math.round(xLocal[i] * sinTheta + yLocal[i] * cosTheta + yc);
+        }
+        return yr;
     }
 
 }
