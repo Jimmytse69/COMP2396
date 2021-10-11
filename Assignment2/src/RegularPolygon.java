0b8cd25c73e91f31a010cedf9c224faacf1e6cd2
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-10-12 05:31:06 +0800

    Update RegularPolygon.java
    update ReurlarPolygon

--- Assignment2/src/RegularPolygon.java
+++ Assignment2/src/RegularPolygon.java
@@ -1,20 +1,16 @@
-/**
- * This the RegularPolygon class to model Regular Polygon shapes under the inheritance of all shapes instance variable and methods
- * there are extra methods/variable need to be overwritten stated as follow. It have 3 Constructors to create this object from class:
- * (), (int n), (int n, double r)
- * @author Tse Chung Wan, 3035689324
- * @version 1.0
- */
-
-public class RegularPolygon extends Shape{
-    public RegularPolygon(){};
-    public RegularPolygon(int n){
-        this.numOfSides = n;
-    };
+class RegularPolygon extends Shape{
     public RegularPolygon(int n, double r){
-        this.numOfSides = n;
-        this.radius = r;
-    };
+        setNumOfSides(n);
+        setRadius(r);
+    }
+    public RegularPolygon(int n){
+        setNumOfSides(n);
+        setRadius(1.0);
+    }
+    public RegularPolygon(){
+        setNumOfSides(3);
+        setRadius(1.0);
+    }
 
     private int numOfSides;
     private double radius;
@@ -25,63 +21,37 @@ public class RegularPolygon extends Shape{
     public double getRadius(){
         return this.radius;
     }
+
+
     public void setNumOfSides(int n){
-        if (n <= 3){
-            this.numOfSides = 3;
-        }
-        else{
-            this.numOfSides = n;
-        }
-        /**
-         *         double[] ResetXLocal = new double[this.getXLocal().length];     //Reset Xlocal
-        for (int i = 0; i < ResetXLocal.length; i++){
-            ResetXLocal[i] = 0;
-        }
-        double[] ResetYLocal = new double[this.getYLocal().length];     //Reset Ylocal
-        for (int i = 0; i < ResetYLocal.length; i++){
-            ResetYLocal[i] = 0;
-        }
-         */
-
-
+        this.numOfSides = n < 3 ? 3 : n;    //one liner if-else
+        setVertices();      //reset Vertices()
     }
+
     public void setRadius(double r){
-        if (r <= 0){
-            this.radius = 0;
-        }
-        else{
-            this.radius = r;
-        }    
-        /**
-         *       double[] ResetXLocal = new double[this.getXLocal().length];     //Reset Xlocal
-        for (int i = 0; i < ResetXLocal.length; i++){
-            ResetXLocal[i] = 0;
-        }
-        double[] ResetYLocal = new double[this.getYLocal().length];     //Reset Ylocal
-        for (int i = 0; i < ResetYLocal.length; i++){
-            ResetYLocal[i] = 0;
-        }
-         *  */    
-  
+        this.radius = r < 0 ? 0 : r;    //one liner if-else
+        setVertices();      //reset Vertices()
     }
-    public void setVertices(){
-        final double theta = 2*(Math.PI)/numOfSides;
-        final double alphaOdd = 0;
-        final double alphaEven = Math.PI/numOfSides;
 
-        if (this.numOfSides % 2 == 1){      //odd sides case
-            for (int i = 0; i < numOfSides; i++){
-                this.getXLocal()[i] = this.getRadius()*(Math.cos(alphaOdd - i*theta));
-                this.getYLocal()[i] = this.getRadius()*(Math.sin(alphaOdd - i*theta));
-            }
+    private void setVertices(){
+        double alpha;
+        double theta;
+        double r;
+        double xVer[] = new double[this.getNumOfSides()];
+        double yVer[] = new double[this.getNumOfSides()];
+
+        alpha = (this.getNumOfSides() % 2 == 1) ? 0 : Math.PI/this.getNumOfSides();
+        theta = (2*Math.PI)/this.getNumOfSides();
+        r = this.getRadius();
+
+        for (int i = 0; i < this.getNumOfSides(); ++i){
+            xVer[i] = r*(Math.cos(alpha-(i*theta)));
+            yVer[i] = r*(Math.sin(alpha-(i*theta)));
         }
-        else{
-            for (int i = 0; i < numOfSides; i++){
-                this.getXLocal()[i] = this.getRadius()*(Math.cos(alphaEven - i*theta));
-                this.getYLocal()[i] = this.getRadius()*(Math.sin(alphaEven - i*theta));
-            }
-        }
+        this.setXLocal(xVer);
+        this.setYLocal(yVer);
     }
+
     //target find min(leftest) x coordinate of vertices, than use the method in appendix
     public boolean contains(double x, double y){
         double[] xL = this.getXLocal();
@@ -99,7 +69,7 @@ public class RegularPolygon extends Shape{
                 x = x*Math.cos(2*Math.PI/numOfSides) - y*Math.sin(2*Math.PI/numOfSides);    //rotate the point
             }
         }
-        return true;
+         return true;
     }
-    
+
 }
