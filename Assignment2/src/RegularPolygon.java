6f43a9bd62559b73ffad65e3d570fa61658cd877
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-10-12 08:49:34 +0800

    Javadoc

--- Assignment2/src/RegularPolygon.java
+++ Assignment2/src/RegularPolygon.java
@@ -2,45 +2,80 @@
  * This RegularPolygon class is used to model general RegularPolygon objects, the detail instance variable and methods are shown below.
  * @author Tse Chung Wan, 3035689324
  * @version 1.2: fixed the problem of method contain(): contain is true even sometimes mouse right click is outside the shape. 
- * This bug is caused by rotate pointXLocal overwritten each, so it is not actually rotating from 1 - n.
+ * This bug is caused by rotate pointXLocal overwritten each iteration, so it is not actually rotating from 1 - n.
  * fixed by final double fixedPointXLocal and final double fixedPointYLocal to avoid it happen.
  */
 
 class RegularPolygon extends Shape{
+    /**
+     * a constructor for building a regular n-sided polygon with radius of r
+     * @param n (int type), num of sides of polygon
+     * @param r (double type), Radius of that polygon
+     */
     public RegularPolygon(int n, double r){
         setNumOfSides(n);
         setRadius(r);
     }
+
+    /**
+     * a constructor for building a regular n-sided polygon with radius r set to 1.0.
+     * @param n (int type), num of sides of polygon
+     */
     public RegularPolygon(int n){
         setNumOfSides(n);
         setRadius(1.0);
     }
+
+    /**
+     * a constructor for building a regular 3-sided polygon with radius r set to 1.0.
+     */
     public RegularPolygon(){
         setNumOfSides(3);
         setRadius(1.0);
     }
 
+    /** private attribute represent that polygon's number of sides */
     private int numOfSides;
+    /** private attribute represent that polygon's radius */
     private double radius;
 
+    /**
+     * getter of numOfSides attribute.
+     * @return numOfSides, int type.
+     */
     public int getNumOfSides(){
         return this.numOfSides;
     }
+
+    /**
+     * getter of radius attribute.
+     * @return radius, double type.
+     */
     public double getRadius(){
         return this.radius;
     }
 
-
+    /**
+     * setter of numOfSides attribute.
+     * @param n (int type)
+     */
     public void setNumOfSides(int n){
         this.numOfSides = n < 3 ? 3 : n;    //one liner if-else
         setVertices();      //reset Vertices()
     }
 
+    /**
+     * setter of radius attribute.
+     * @param r (double type)
+     */
     public void setRadius(double r){
         this.radius = r < 0 ? 0 : r;    //one liner if-else
         setVertices();      //reset Vertices()
     }
 
+    /**
+     * private method Setter set Vertices in terms of local coordinates of the regular n-sided polygon based on its number of sides and radius
+     */
     private void setVertices(){     //should be private bacuse normally won't "reset" Vertices, but reset by constructor or other setter in this class etc.
         final double theta = 2*(Math.PI)/this.numOfSides;
         final double alphaOdd = 0;
@@ -66,6 +101,12 @@ class RegularPolygon extends Shape{
     }
 
 
+    /**
+     * a method for determining if a point (x, y) in the screen coordinate system is contained by the regular n-sided polygon.
+     * @param x (double type)
+     * @param y (double type)
+     * @return boolean value, true => (x,y) contains in that Polygon
+     */
     //target find min(leftest) x coordinate of vertices, than use the method in appendix
     public boolean contains(double x, double y){
         final double theta = 2*(Math.PI)/this.numOfSides;
--- Assignment2/src/Shape.java
+++ Assignment2/src/Shape.java
@@ -7,65 +7,151 @@ import java.awt.Color;
  */
 
 public class Shape {
+    /** a Color object specifying the color of the shape. */
     private Color color;
+
+    /** a boolean value specifying whether the shape is filled or not filled. */
     private boolean filled;
-    private double theta;   //radian, Screen Coordinate system(SCS)
-    private double xc;  //SCS
-    private double yc;  //SCS
+
+    /** a double value specifying the orientation (in radians) of the shape in the 
+screen coordinate system */
+    private double theta;
+
+    /** a double value specifying the x-coordinate of the center of the shape in the 
+screen coordinate system. */
+    private double xc;  
+
+    /** a double value specifying the y-coordinate of the center of the shape in the 
+screen coordinate system. */
+    private double yc; 
+
+    /** an  array  of  double  values  specifying  the  x-coordinates  of  the 
+vertices (in counter-clockwise order) of the shape in its local coordinate system.  */
     private double[] xLocal;
+
+    /** an  array  of  double  values  specifying  the  y-coordinates  of  the 
+vertices (in counter-clockwise order) of the shape in its local coordinate system.  */
     private double[] yLocal;
 
+    /** getter method of retriving instance variable of Color in that object 
+     * @return {Color} color of that obj.
+    */
     public Color getColor(){
         return this.color;
     }
+
+    /** getter method of retriving instance variable of filled in that object 
+     * @return {Boolean} filled of that obj.
+    */
     public boolean getFilled(){
         return this.filled;
     }
+
+    /** getter method of retriving instance variable of theta in that object 
+     * @return {double} theta of that obj.
+    */
     public double getTheta(){
         return this.theta;
     }
+
+    /** getter method of retriving instance variable of xc in that object 
+     * @return {double} X coordinate of center of that obj.
+    */
     public double getXc(){
         return this.xc;
     }
+
+    /** getter method of retriving instance variable of yc in that object 
+     * @return {double} Y coordinate of center of that obj.
+    */
     public double getYc(){
         return this.yc;
     }
+
+    /** getter method of retriving instance variable of XLocal double array in that object 
+     * @return {double[]} X coordinates of Vertices of that obj in Local coor. sys..
+    */
     public double[] getXLocal(){
         return this.xLocal;
     }
+
+    /** getter method of retriving instance variable of YLocal double array in that object 
+     * @return {double[]} Y coordinates of Vertices of that obj in Local coor. sys..
+    */    
     public double[] getYLocal(){
         return this.yLocal;
     }
 
+
+    /** setter method of set color attribute in that object 
+     * @param color (Color type) represented in RGB value, ref: https://docs.oracle.com/javase/8/docs/api/java/awt/Color.html
+    */       
     public void setColor(Color color){
         this.color = color;
     }
+
+    /** setter method of set filled attribute in that object 
+     * @param filled (boolean type)
+    */     
     public void setFilled(boolean filled){
         this.filled = filled;
     }
+
+    /** setter method of set theta attribute in that object 
+     * @param theta (double type), it is in radian from positive x-axis of Screen coordinate
+    */
     public void setTheta(double theta){
         this.theta = theta;
     }
+
+    /** setter method of set x center attribute in that object 
+     * @param xc (double type)
+    */
     public void setXc(double xc){
         this.xc = xc;
     }
+
+    /** setter method of set y center attribute in that object 
+     * @param yc (double type)
+    */
     public void setYc(double yc){
         this.yc = yc;
     }
+
+    /** setter method of set x Vertices attribute in that object 
+     * @param xLocal (double[] type), it is in Local coordinate system in terms of counter-clockwise order
+    */
     public void setXLocal(double[] xLocal){
         this.xLocal = xLocal;
     }
+
+    /** setter method of set y Vertices attribute in that object 
+     * @param yLocal (double[] type), it is in Local coordinate system in terms of counter-clockwise order
+    */
     public void setYLocal(double[] yLocal){
         this.yLocal = yLocal;
     }
+
+    /** a method to translate center coordinate by +dy and +dy.
+     * @param dx (double type) amount of x to translate
+     * @param dy (double type) amount of y to translate
+    */
     public void translate(double dx, double dy){
         xc += dx;
         yc += dy;
     }
+
+    /** a method to rotate the shape by dt in radian.
+     * @param dt (double type), rotation angle
+    */
     public void rotate(double dt){
         theta += dt;
     }
 
+    /**
+     * a method for retrieving the x-coordinates of the vertices (in counter-clockwise order).
+     * @return screen coordinate of X vertices of the shape rounded to nearest intergers.
+     */
     public int[] getX(){
         double[] screenXLocal = new double[xLocal.length];
         int[] roundedScreenXLocal = new int[xLocal.length];
@@ -77,6 +163,11 @@ public class Shape {
         }
         return roundedScreenXLocal;
     }
+
+    /**
+     * a method for retrieving the y-coordinates of the vertices (in counter-clockwise order).
+     * @return screen coordinate of Y vertices of the shape rounded to nearest intergers.
+     */
     public int[] getY(){
         double[] screenYLocal = new double[yLocal.length];
         int[] roundedScreenYLocal = new int[yLocal.length];
