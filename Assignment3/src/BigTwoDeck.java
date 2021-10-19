dfb7e65ff600e56b9dc57e118ad6b48ba877dcc9
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-10-19 17:48:03 +0800

    skeleton of BigTwoDeck class

---
+++ Assignment3/src/BigTwoDeck.java
@@ -0,0 +1,17 @@
+/** 
+ * This BigTwoDeck class is a subclass of the Deck class to model a deck of cards used in a Big Two game.
+ * The overwritten methods as detailed as below
+ * @author Tse Chung Wan, 3035689324
+ * @version 1.0
+ * @date 19/10/2021 (start ver1.0)
+ */
+
+public class BigTwoDeck extends Deck{
+    /**
+     * a public overriding method for initializing a deck of Big Two cards.
+     * it will remove all cards from the deck, create 52 Big Two cards and add them to the deck.
+     */
+    public void initialize(){
+        
+    }
+}
