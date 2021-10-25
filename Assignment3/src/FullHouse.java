6beff2e4bb09a86309b4b7947fb32755e5176cce
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-10-25 19:33:19 +0800

    FullHouse v1.0

--- Assignment3/src/FullHouse.java
+++ Assignment3/src/FullHouse.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * This FullHouse is subclass of Hand class
  * @author Tse Chung Wan, 3035689324
@@ -19,11 +21,26 @@ public class FullHouse extends Hand{
      * @return boolean true = valid
     */
     public boolean isValid(){
+        ArrayList<Integer> freqOfRank = new ArrayList<Integer>(); 
+        if (this.size() == 5){
+            for (int i = 0; i < this.size(); ++i){
+                freqOfRank.add(this.getCard(i).getRank());
+            }
+            //sorted order of freqOfRank. the (first 3 elem should have same rank and last 2 elem should have same rank) or
+            //                                (first 2 elem should have same rank and last 3 elem should have same rank)
+            Collections.sort(freqOfRank);
+            if ( ( (freqOfRank.get(0) == freqOfRank.get(1) && freqOfRank.get(1) == freqOfRank.get(2)) && (freqOfRank.get(3) == freqOfRank.get(4)) ) ||
+                 ( (freqOfRank.get(1) == freqOfRank.get(1)) && (freqOfRank.get(2) == freqOfRank.get(3) && freqOfRank.get(3) == freqOfRank.get(4)) ) ){
+                    return true;
+            }
+        }
+        return false;
     }
         
     /**Type of this name 
      * @return string of hand type name
     */
     public String getType(){
+        return "FullHouse";
     }
 }
