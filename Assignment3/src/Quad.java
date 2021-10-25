48a1307b7cfef6cb8df299a473496f52fc8dfb61
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-10-25 19:45:50 +0800

    Quad v1.0

--- Assignment3/src/Quad.java
+++ Assignment3/src/Quad.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * This Quad is subclass of Hand class
  * @author Tse Chung Wan, 3035689324
@@ -19,11 +21,41 @@ public class Quad extends Hand{
      * @return boolean true = valid
     */
     public boolean isValid(){
+        boolean first4 = false;
+        boolean last4 = false;
+        int first4Count = 0;
+        int last4Count = 0;
+        ArrayList<Integer> freqOfRank = new ArrayList<Integer>(); 
+        if (this.size() == 5){
+            for (int i = 0; i < this.size(); ++i){
+                freqOfRank.add(this.getCard(i).getRank());
+            }
+            //sorted order of freqOfRank. the (first4 should have same rank) or (last 4 should have same rank)
+            Collections.sort(freqOfRank);
+            for (int j = 0; j < this.size() - 2; ++j){
+                if (freqOfRank.get(j) == freqOfRank.get(j+1)){
+                    first4Count++;  //0-1, 1-2, 2-3
+                }
+            }
+            for (int k = 1; k < this.size() - 1; ++k){
+                if (freqOfRank.get(k) == freqOfRank.get(k+1)){
+                    last4Count++;   //1-2, 2-3, 3-4
+                }
+            }
+            if (first4Count != 3 && last4Count != 3){
+                return false;
+            }
+            else{
+                return true;    //at least 1 == 3
+            }
+        }
+        return false;
     }
         
     /**Type of this name 
      * @return string of hand type name
     */
     public String getType(){
+        return "Quad";
     }
 }
