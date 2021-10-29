771e7db22d5d1cfea84a1c7928797e788a9eeb73
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2021-10-29 21:32:36 +0800

    T6

---
+++ Tutorial/COMP2396A-Tutorial6/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        final int a;
+        a = 1;
+        a = 2;
+    }
+}
